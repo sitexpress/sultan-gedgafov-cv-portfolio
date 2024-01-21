365deddd426d5afb35472d3d109c9e16b03e0312
Sultan <sitexpressorg@gmail.com>
2024-01-21 23:01:05 +0300

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -46,17 +46,80 @@ window.addEventListener('DOMContentLoaded', event => {
     "Experience",
     "Опыт",
 
-    "Working on an internal project:",
-    "Работа над внутренним проектом:",
-
-    "November 2022 - November 2023",
-    "Ноябрь 2022 - Ноябрь 2023",
-
     "Development of multi-platform websites. HTML, CSS, SCSS, Gulp, JavaScript, etc.",
     "Разработка мультиплатформенных сайтов. HTML, CSS, SCSS, Gulp, JavaScript и т.д.",
     
     "April 2022 - August 2022",
     "Апрель 2022 - Август 2022",
+
+
+    
+    "Manager",
+    "Менеджер",
+
+    "SELF-EMPLOYED (QUESTROOM)",
+    "Самозанятый (QUESTROOM)",
+
+    "Control of all aspects of small business, management, customer acquisition, construction, multi-platform website creation, SEO management and optimization, organization events.",
+    "Контроль всех аспектов малого бизнеса, управление, привлечение клиентов, строительство, создания мультиплатформенного веб-сайта, управления SEO и оптимизация, организация мероприятий",
+
+    "April 2015 - April 2022",
+    "Апрель 2015 - Апрель 2022",
+
+
+
+    "PROJECT MANAGER AT OLYMPICS SOCHI 2014",
+    "Менеджер проектов на Олимпийских играх в Сочи 2014",
+
+    "Monitoring of temporary buildings and infrastructures, control of the installation of tents, modules and temporary infrastructure systems at Olympic venues, such as Main Media Center (MMC) and Mountain Olympic Village (MOV). Monitoring compliance with schedules.  ",
+    "Мониторинг временных зданий и инфраструктур, контроль установки палаток, модулей и систем временной инфраструктуры на олимпийских объектах, таких как Главный Медиа Центр (MMC) и Горная олимпийская деревня (МОВ). Контроль соблюдения графиков.",
+
+    "October 2013 - April 2014",
+    "Октябрь 2013 - Апрель 2014",
+
+
+
+    "Lead Engineer",
+    "Ведущий инженер",
+
+    "Volgomost",
+    "АО Волгомост",
+
+    "Control of delivery of all structures to sites. Control of drawings. Control production work. Oversaw the construction of a bridge across the Sochi River. Supervised construction of several bridges on the M4-Don Sochi-Krasnodar highway.",
+    "Контроль доставки всех конструкций на объекты. Контроль чертежей. Контроль производственных работ. Курировал строительство моста через реку Сочи. Курировал строительство нескольких мостов на трассе М4-Дон Сочи-Краснодар.",
+
+    "November 2012 - October 2013",
+    "Ноябрь 2013 - Октябрь 2014",
+
+
+
+    "Foreman",
+    "Производитель работ",
+
+    "LLC Svatstroy",
+    "ООО Сватстрой",
+
+    "Project management and work execution at one of the sites.",
+    "Управление проектом и производством работ на одном из участков.",
+
+    "April 2010 - November 2012",
+    "Апрель 2010 - Ноябрь 2012",
+
+
+
+    "Draftsman",
+    "Инженер ПТО",
+
+    "ARKO",
+    "АРКО",
+
+    "Creation of sketches, plans, drawings, etc. in the building company.",
+    "Создание эскизов, планов, чертежей и т.д. в Строительной компании.",
+
+    "March 2007 - April 2010",
+    "Март 2007 - Апрель 2010",
+
+
 
     "Education",
     "Образование",
